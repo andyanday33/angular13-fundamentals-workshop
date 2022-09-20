7582899d23de37484e678ded734c5cbb7292edc6
berke <bab26@st-andrews.ac.uk>
2022-09-20 13:11:24 +0300

    feat: Added course listings with select and delete functionality

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,22 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  // RENDER COURSES IN A LIST
+  // SELECT A COURSE
+  // RENDER SELECTED COURSE
+
   courses = [
     {
       id: 1,
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'JS: The Hard Parts',
+      description: 'Learn the hard parts of Javacript.',
+      percentComplete: 33,
+      favorite: true,
+    },
   ];
+  selectedCourse = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
 
+  deleteCourse(id: number) {
+    this.courses = this.courses.filter((course) => course.id !== id);
+  }
 }
